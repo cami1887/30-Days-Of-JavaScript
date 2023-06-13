76b994c163da7a07c3416b961d82419b467cd029
Camille Lacaden <cami1887@gmail.com>
2023-06-13 11:36:34 -0700

    completed day 14

--- 14_Day_Error_handling/14_day_starter/scripts/main.js
+++ 14_Day_Error_handling/14_day_starter/scripts/main.js
@@ -1,2 +1,28 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// Practice
+
+    // try {
+    //     console.log(firstName)
+    // }
+    // catch (err) {
+    //     console.log(err)
+    // }
+    // finally {
+    //     console.log('output should be Camille Lacaden')
+    // }
+
+
+    // const throwErrorExampleFun = () => {
+    //     let message = 'Great';
+    //     let x = prompt('Enter a number: ')
+    //     try {
+    //       if (x == '') throw 'field is empty'
+    //       if (isNaN(x)) throw 'not a number'
+    //       x = Number(x)
+    //       if (x < 5) throw 'too low'
+    //       if (x > 10) throw 'too high'
+    //     } catch (err) {
+    //       console.log(err)
+    //     }
+    //     console.log(message);
+    //   }
+    //   throwErrorExampleFun()
